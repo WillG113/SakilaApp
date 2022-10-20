7b5356b2c60ed22e929484deb20d31441f0ac604
WillG <will.g.garner@googlemail.com>
2022-10-20 12:30:14 +0100

    Added Jacoco

--- src/test/java/com/Sakila/api/SakilaApp/SakilaAppApplicationTests.java
+++ src/test/java/com/Sakila/api/SakilaApp/SakilaAppApplicationTests.java
@@ -1,6 +1,6 @@
 package com.Sakila.api.SakilaApp;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
