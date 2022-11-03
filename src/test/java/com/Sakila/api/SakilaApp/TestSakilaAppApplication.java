ad75f7679d5033302806055b81a8843e00ac5e28
WillG <will.g.garner@googlemail.com>
2022-11-03 11:25:21 +0000

    Attempting to test some getmapping requests

--- src/test/java/com/Sakila/api/SakilaApp/TestSakilaAppApplication.java
+++ src/test/java/com/Sakila/api/SakilaApp/TestSakilaAppApplication.java
@@ -5,11 +5,16 @@ import org.json.*;
 import org.junit.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.*;
+import org.mockito.*;
+import org.mockito.junit.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.*;
 import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.mock.mockito.*;
 import org.springframework.http.*;
 import org.springframework.mock.web.*;
+import org.springframework.test.context.junit4.*;
 import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.request.*;
 import org.springframework.test.web.servlet.result.*;
@@ -23,28 +28,26 @@ import java.util.*;
 
 public class TestSakilaAppApplication {
 
+    ActorRepository actorRepository = mock(ActorRepository.class);
+    FilmRespository filmRespository = mock(FilmRespository.class);
+    CategoryRepository categoryRepository = mock(CategoryRepository.class);
+    FilmActorRepository filmActorRepository = mock(FilmActorRepository.class);
+    CategoryFilmRepository categoryFilmRepository = mock(CategoryFilmRepository.class);
+    SakilaAppApplication mockApp = new SakilaAppApplication(actorRepository, filmRespository, categoryRepository, filmActorRepository, categoryFilmRepository);
 
-
-    FilmRespository filmRepo = mock(FilmRespository.class);
-    ActorRepository actorRepo = mock(ActorRepository.class);
-    FilmActorRepository filmActorRepo = mock(FilmActorRepository.class);
-    CategoryRepository categoryRepo = mock(CategoryRepository.class);
-    CategoryFilmRepository categoryFilmRepo = mock(CategoryFilmRepository.class);
-    SakilaAppApplication mockApp = new SakilaAppApplication(actorRepo, filmRepo, categoryRepo, filmActorRepo, categoryFilmRepo);
-    SakilaAppApplication mockApp2 = mock(SakilaAppApplication.class);
-
+    Film testFilm = new Film("testTitle", "testDesc", "2006", 5, 0.99, 25, 20.99, "PG");
+    Film testFilm2 = new Film("testTitle2", "testDesc", "2006", 5, 0.99, 25, 20.99, "PG");
+    Iterable<Film> allFilms = Arrays.asList(testFilm, testFilm2);
 
     @Test
-    public void testGetMapping() throws Exception {
-        Film f = new Film("testTitle", "testDesc", "2006", 5, 0.99, 25, 20.99, "PG");
+    public void testGetMapping() {
 
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        Mockito.when(mockApp.getAllFilmsAPI()).thenReturn(allFilms);
 
-        when(mockApp2.getFilmAPI(5)).thenReturn(f);
-        Film responseBody = mockApp2.getFilmAPI(5);
+        Iterable<Film> actualResult = mockApp.getAllFilmsAPI();
 
-        Assertions.assertEquals(f, responseBody);
+        Assertions.assertEquals(allFilms, actualResult);
+
     }
 
 
