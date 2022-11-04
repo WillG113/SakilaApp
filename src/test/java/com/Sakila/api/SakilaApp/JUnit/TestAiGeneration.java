0903dba76cd2c2e0c77eecbf93aa94f2e81f5794
WillG <will.g.garner@googlemail.com>
2022-11-04 11:39:19 +0000

    Mockito tests for AI Generation

--- src/test/java/com/Sakila/api/SakilaApp/JUnit/TestAiGeneration.java
+++ src/test/java/com/Sakila/api/SakilaApp/JUnit/TestAiGeneration.java
@@ -46,6 +46,31 @@ public class TestAiGeneration {
     }
 
     @Test
+    public void testFetchMethod2() throws JSONException, InterruptedException {
+
+        AiGeneration ai = new AiGeneration();
+
+        String dummyResponse = "{\"finished\": 0, \"processing\": 0, \"restarted\": 0, \"waiting\": 0, \"done\": true, \"faulted\": false, \"wait_time\": 0, \"queue_position\": 0, \"kudos\": 10.83, \"is_possible\": true}";
+
+        String dummyResponse2 = "{\"finished\": 1, \"processing\": 0, \"restarted\": 0, \"waiting\": 0, \"done\": true, \"faulted\": false, \"wait_time\": 0, \"queue_position\": 0, \"kudos\": 10.83, \"is_possible\": true}";
+
+        String otherDummyResponse = "{\"generations\": [{\"img\": \"1234\", \"seed\": \"3603702987\", \"worker_id\": \"1405eb95-9d06-4fe6-8d67-db554a378951\", \"worker_name\": \"Angulimala\", \"model\": \"stable_diffusion\"}], \"finished\": 1, \"processing\": 0, \"restarted\": 0, \"waiting\": 0, \"done\": true, \"faulted\": false, \"wait_time\": 0, \"queue_position\": 0, \"kudos\": 10.83, \"is_possible\": true}\n";
+
+        ResponseEntity<String> dummy = new ResponseEntity<String>(dummyResponse, HttpStatus.ACCEPTED);
+        ResponseEntity<String> dummy2 = new ResponseEntity<String>(otherDummyResponse, HttpStatus.ACCEPTED);
+        ResponseEntity<String> dummy3= new ResponseEntity<String>(dummyResponse2, HttpStatus.ACCEPTED);
+
+        ai.restTemplate = mock(RestTemplate.class);
+
+        when(ai.restTemplate.getForEntity("https://stablehorde.net/api/v2/generate/check/a", String.class)).thenReturn(dummy).thenReturn(dummy3);
+        when(ai.restTemplate.getForEntity("https://stablehorde.net/api/v2/generate/status/a", String.class)).thenReturn(dummy2);
+
+        String actual = ai.fetchMethod("a");
+
+        Assertions.assertEquals("1234", actual);
+    }
+
+    @Test
     public void testPostMethod() throws JSONException {
 
 
