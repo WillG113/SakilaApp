3e0104a6b0c14e4b6a59a8da7f6adf5b1f821484
WillG <will.g.garner@googlemail.com>
2022-11-04 12:54:38 +0000

    Mockito tests for AI Generation

--- src/test/java/com/Sakila/api/SakilaApp/JUnit/TestSakilaAppApplication.java
+++ src/test/java/com/Sakila/api/SakilaApp/JUnit/TestSakilaAppApplication.java
@@ -1,37 +1,20 @@
 package com.Sakila.api.SakilaApp.JUnit;
 
+
 import com.Sakila.api.SakilaApp.*;
-import org.json.*;
-import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.*;
-import org.mockito.*;
-import org.mockito.junit.*;
-import org.springframework.boot.*;
-import org.springframework.boot.builder.*;
 import org.springframework.boot.test.context.*;
-import org.springframework.context.*;
 import org.springframework.test.context.junit4.*;
-import org.springframework.web.servlet.*;
 
-import static org.mockito.Mockito.*;
-
-import javax.servlet.*;
-import java.util.*;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
 
 public class TestSakilaAppApplication {
-
+        
         @Test
-        public void applicationContextLoaded() {
-        }
+        public void test() {
 
-        @Test
-        public void applicationContextTest() {
-                SakilaAppApplication.main(new String[] {});
+                //String array[] = { };
+                //SakilaAppApplication.main(array);
         }
 
 }
