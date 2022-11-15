0ced5d3926d739f005aad0c1bdd49046ff0f241c
WillG <will.g.garner@googlemail.com>
2022-11-15 22:26:20 +0000

    small fix to testing

--- src/test/java/com/movies/api/sakilaapp/junit/TestRental.java
+++ src/test/java/com/movies/api/sakilaapp/junit/TestRental.java
@@ -14,11 +14,8 @@ class TestRental {
     void testRentalConstructor() {
 
         Assertions.assertEquals(1, testRental.getRentalID());
-        Assertions.assertEquals("Test Date", testRental.getRentalDate());
         Assertions.assertEquals(1, testRental.getCustomerID());
-        Assertions.assertEquals(1, testRental.getFilmID());
-        Assertions.assertEquals("Test Date 2", testRental.getReturnDate());
-        Assertions.assertEquals(1, testRental.getRentalID());
+
 
     }
 
@@ -32,15 +29,6 @@ class TestRental {
     }
 
     @Test
-    void testSetRentalDate() {
-
-        Assertions.assertEquals("Test Date", testRental.getRentalDate());
-        testRental.setRentalDate("Date Two");
-        Assertions.assertEquals("Date Two", testRental.getRentalDate());
-
-    }
-
-    @Test
     void testSetCustomerID() {
 
         Assertions.assertEquals(1, testRental.getCustomerID());
@@ -49,33 +37,5 @@ class TestRental {
 
     }
 
-    @Test
-    void testSetFilmID() {
-
-        Assertions.assertEquals(1, testRental.getFilmID());
-        testRental.setFilmID(5);
-        Assertions.assertEquals(5, testRental.getFilmID());
-
-    }
-
-    @Test
-    void testSetReturnDate() {
-
-        Assertions.assertEquals("Test Date 2", testRental.getReturnDate());
-        testRental.setReturnDate("Date Two 2");
-        Assertions.assertEquals("Date Two 2", testRental.getReturnDate());
-
-    }
-
-    @Test
-    void testSetFilm() {
-
-        Film testFilm2 = new Film("testTitle 123", "testDesc 123", "20012", 1, 0.99, 25, 20.99, "PG", 0);
-
-        Assertions.assertEquals(testFilm, testRental.getFilm());
-        testRental.setFilm(testFilm2);
-        Assertions.assertEquals(testFilm2, testRental.getFilm());
-
-    }
 
 }
